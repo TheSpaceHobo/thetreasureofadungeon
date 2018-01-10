ee490d44187937032790740b0481749bab2030ea
TheSpaceHobo <32850835+TheSpaceHobo@users.noreply.github.com>
2018-01-10 12:53:21 -0700

    Update scriptexperiment.js

--- scriptexperiment.js
+++ scriptexperiment.js
@@ -20,23 +20,23 @@ function treasureitem(id, name, price, rarity, texture){
   }
 }
 
-var antimatterkatana = new treasureitem("antikatana","Antimatter_Katana",150,"Legendary","images/Anitmatter-Katana.gif");
-var dubloon = new treasureitem("coin","Dubloon",10,"Common","images/dubloon256.gif");
-var chalice = new treasureitem("goldcup","Chalice",20,"Common","images/chalice.gif");
-var goldbar = new treasureitem("goldbar","Bar_Of_Gold",50,"Uncommon","images/goldbar.gif");
-var diamond = new treasureitem("diamond","Diamond",45,"Uncommon","images/diamond.gif");
-var ruby = new treasureitem("ruby","Ruby",45,"Uncommon","images/ruby.gif");
-var emerald = new treasureitem("emerald","Emerald",45,"Uncommon","images/emerald.gif");
-var obsidiansphere = new treasureitem("shpereofobsidian","Obsidian_Sphere",600,"Mythical","images/obsidiansphere.gif");
-var eyeofoni = new treasureitem("eyeofoni","The_Eye_Of_The_Oni",600,"Mythical","images/eyeofoni.gif");
-var arcanecodex = new treasureitem("codex","Arcane_Codex",300,"Legendary","images/codex.gif");
-var durasteelkatana = new treasureitem("durasteelkatana","Durasteel_Katana",50,"Uncommon","images/durasteelkatana.gif");
-var argoncrystalkatana = new treasureitem("argonkatana","Argon_Crystal_Katana",200,"Very Rare","images/argonkatana.gif");
-var heliumcrystalkatana = new treasureitem("heliumkatana","Helium_Crystal_Katana",200,"Very Rare","images/heliumkatana.gif");
-var practicekatana = new treasureitem("woodkatana","Practice_Katana",10,"Common","images/practicekatana.gif");
-var ironkatana = new treasureitem("ironkatana","Katana",25,"Uncommon","images/ironkatana.gif");
-var diamondkatana = new treasureitem("diamondkatana","Diamond_Katana",200,"Very Rare","images/diamondkatana.gif");
-var obsidiankatana = new treasureitem("obsidiankatana","Obsidian_Katana",200,"Very Rare","images/obsidiankatana.gif");
+var antimatterkatana = new treasureitem("antikatana","Antimatter_Katana",150,"Legendary","Anitmatter-Katana.gif");
+var dubloon = new treasureitem("coin","Dubloon",10,"Common","dubloon256.gif");
+var chalice = new treasureitem("goldcup","Chalice",20,"Common","chalice.gif");
+var goldbar = new treasureitem("goldbar","Bar_Of_Gold",50,"Uncommon","goldbar.gif");
+var diamond = new treasureitem("diamond","Diamond",45,"Uncommon","diamond.gif");
+var ruby = new treasureitem("ruby","Ruby",45,"Uncommon","ruby.gif");
+var emerald = new treasureitem("emerald","Emerald",45,"Uncommon","emerald.gif");
+var obsidiansphere = new treasureitem("shpereofobsidian","Obsidian_Sphere",600,"Mythical","obsidiansphere.gif");
+var eyeofoni = new treasureitem("eyeofoni","The_Eye_Of_The_Oni",600,"Mythical","eyeofoni.gif");
+var arcanecodex = new treasureitem("codex","Arcane_Codex",300,"Legendary","codex.gif");
+var durasteelkatana = new treasureitem("durasteelkatana","Durasteel_Katana",50,"Uncommon","durasteelkatana.gif");
+var argoncrystalkatana = new treasureitem("argonkatana","Argon_Crystal_Katana",200,"Very Rare","argonkatana.gif");
+var heliumcrystalkatana = new treasureitem("heliumkatana","Helium_Crystal_Katana",200,"Very Rare","heliumkatana.gif");
+var practicekatana = new treasureitem("woodkatana","Practice_Katana",10,"Common","practicekatana.gif");
+var ironkatana = new treasureitem("ironkatana","Katana",25,"Uncommon","ironkatana.gif");
+var diamondkatana = new treasureitem("diamondkatana","Diamond_Katana",200,"Very Rare","diamondkatana.gif");
+var obsidiankatana = new treasureitem("obsidiankatana","Obsidian_Katana",200,"Very Rare","obsidiankatana.gif");
 var alltreasure = [
   antimatterkatana,
   dubloon,
